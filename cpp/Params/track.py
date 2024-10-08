18ef49e46b46864b84b9bc8443a5dfa74966dcd9
JunHeonYoon <yuhee9700@gmail.com>
2024-10-08 13:06:02 +0900

    update exp track

--- cpp/Params/track.py
+++ cpp/Params/track.py
@@ -3,30 +3,29 @@ import numpy as np
 from scipy.spatial.transform import Rotation as R
 
 # For self collision experiment
-x = [0, -0.20, -0.40, 0]
-y = [0, 0, 0, 0]
-z = [0, -0.51, -0.4,0]
+r = 0.1
+t = np.linspace(pi/2, 8*pi+pi/2, 100)
 
-rot1 = R.from_matrix([[1,  0,  0],
+x = 2.5*r * np.sin(t)
+y = 1.5*r * np.sin(2 * t)
+z = 2.15*r * np.cos(t)
+# z = 0*r * np.cos(t)
+
+rot = R.from_matrix([[1,  0,  0],
                      [0, -1, 0],
                      [0,  0, -1]])
-
-rot2 = R.from_matrix([[0,  0,  -1],
-                     [0, -1, 0],
-                     [-1,  0, 0]])
-quat1 = rot1.as_quat().reshape(1,-1)
-quat2 = rot2.as_quat().reshape(1,-1)
-quat_list = np.concatenate([quat1, quat2, quat2, quat1], axis=0)
+quat = rot.as_quat()
+quat_list = np.tile(quat, (x.size, 1))
 
 
 # For singularity experiment
 # r = 0.1
-# t = np.linspace(0, 2*pi, 100)
+# t = np.linspace(0, 8*pi, 100)
 
-# x = 2.4*r * np.sin(t)
+# x = 2.0*r * np.sin(t)
 # y = 1.5*r * np.sin(2 * t)
-# # z = 2.15*r * np.cos(t)
-# z = 0*r * np.cos(t)
+# z = 2.15*r * np.cos(t)
+# # z = 0*r * np.cos(t)
 
 # rot = R.from_matrix([[1,  0,  0],
 #                      [0, -1, 0],
